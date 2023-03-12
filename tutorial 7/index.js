8de0466070176d635b8cd270762597e23d0bb526
asitkr <krsinhasaurabh@gmail.com>
2023-03-12 09:48:24 +0530

    done

--- tutorial 7/index.js
+++ tutorial 7/index.js
@@ -8,6 +8,24 @@ const PORT = "5000";
 const publicPath = path.join(__dirname, 'public')
 
 // static method load the static file or pages
-app.use(express.static(publicPath))
+// app.use(express.static(publicPath))
+
+// Updating url without file extension
+// sendFile method we can load a file
+app.get('', (req, res) => {
+    res.sendFile(`${publicPath}/index.html`);
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(`${publicPath}/About.html`);
+})
+
+app.get('/help', (req, res) => {
+    res.sendFile(`${publicPath}/Help.html`);
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(`${publicPath}/404.html`);
+})
 
 app.listen(PORT);
