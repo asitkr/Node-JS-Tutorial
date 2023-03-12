9ffca463a42c35ae70d2f271a7965b8df516533b
asitkr <krsinhasaurabh@gmail.com>
2023-03-12 18:07:35 +0530

    done

--- tutorial 8/index.js
+++ tutorial 8/index.js
@@ -1,2 +1,19 @@
 // Template engine
 
+const express = require('express');
+const app = express();
+
+const PORT = "5000";
+
+app.set('view engine', 'ejs');
+
+app.get('/profile', (req, res) => {
+    const user = {
+        name: "Aashit",
+        email: "aashit@gmail.com",
+        city: 'Noida'
+    }
+    res.render('profile', {user});
+})
+
+app.listen(PORT);
---
+++ tutorial 9/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const PORT = "5000";
+
+app.set('view engine', 'ejs');
+
+app.get('/profile', (req, res) => {
+    const user = {
+        name: "Aashit",
+        email: "aashit@gmail.com",
+        city: 'Noida'
+    }
+    res.render('profile');
+})
+
+app.listen(PORT);
