8f74b5817924651f2648f8b39ae4495d8eb0581e
asitkr <krsinhasaurabh@gmail.com>
2023-03-15 16:00:10 +0530

    done

--- tutorial 20/index.js
+++ tutorial 20/index.js
@@ -7,4 +7,15 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/search/:key', async (req, res) => {
+    console.log(req.params.key);
+    let data = await product.find(
+        {"$or": [
+            {"name": {$regex: req.params.key}},
+            {"brand": {$regex: req.params.key}}
+        ]}
+    );
+    res.send(data)
+})
+
 app.listen(PORT);
